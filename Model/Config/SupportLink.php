55573b7e60e5f2f94b2ee45fdb0e7c7c28565389
Basant <basantmandal@gmail.com>
2026-05-11 17:59:11 +0000

    feat: enhance support link functionality and add template for admin configuration

--- Model/Config/SupportLink.php
+++ Model/Config/SupportLink.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace HK2\MarketingAnalytics\Model\Config;
 
 use Magento\Config\Block\System\Config\Form\Field;
@@ -6,16 +8,25 @@ use Magento\Framework\Data\Form\Element\AbstractElement;
 
 class SupportLink extends Field
 {
+    /**
+     * @var string
+     */
+    protected $_template = 'HK2_MarketingAnalytics::system/config/support_link.phtml';
+
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
     protected function _getElementHtml(AbstractElement $element)
     {
-        $supportUrl = 'https://www.basantmandal.in/support';
-        
-        $html = '<div style="padding: 10px; background-color: #fff3cd; border: 1px solid #ffecb5; border-radius: 4px;">';
-        $html .= '<strong>Support:</strong><br/>';
-        $html .= '<a href="' . $supportUrl . '" target="_blank" style="color: #664d03;">' . $supportUrl . '</a>';
-        $html .= '<p style="margin: 5px 0 0 0; color: #666; font-size: 12px;">Get help with installation, configuration, and troubleshooting</p>';
-        $html .= '</div>';
-        
-        return $html;
+        return $this->_toHtml();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSupportUrl(): string
+    {
+        return 'https://www.basantmandal.in/support';
     }
 }
--- registration.php
+++ registration.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * HK2_MarketingAnalytics - Module registration file
  *
